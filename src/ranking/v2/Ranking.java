f70478e11728000cde8c97161540d8c03e16ee5b
tarosuke <felis.silvestris.catus.7777@gmail.com>
2023-08-06 23:28:00 +0900

    add v2

---
+++ src/ranking/v2/Ranking.java
@@ -0,0 +1,118 @@
+package ranking.v2;
+
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.toMap;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class Ranking {
+
+  public static void main(String[] args) throws IOException {
+
+    var gameEntryLogPath = Paths.get(args[0]);
+    var gameScoreLogPath = Paths.get(args[1]);
+
+    Map<String, String> gameEntryLogData = gameEntryLogData(gameEntryLogPath);
+    List<String> gameScoreLogData = gameScoreLogData(gameScoreLogPath);
+
+    Map<String, Integer> playerLogData = getPlayLogData(gameScoreLogData);
+
+    playerLogData = sortPlayLogData(playerLogData);
+
+    List<String> rankingData = getRankingData(playerLogData, gameEntryLogData);
+
+    outputRankingData(rankingData);
+  }
+
+  /**
+   * playerId毎にスコアを集計したプレイヤーログデータを取得
+   * 
+   * @param scoreLogFilePath
+   * @return プレイヤーログデータ
+   */
+  private static Map<String, String> gameEntryLogData(Path gameEntryLogPath) throws IOException {
+    try (Stream<String> lines = Files.lines(gameEntryLogPath)) {
+      return lines.skip(1).map(line -> line.split(","))
+          .collect(toMap(values -> values[0], values -> values[1]));
+    }
+  }
+
+  /**
+   * playerId毎にスコアを集計したプレイヤーログデータを取得
+   * 
+   * @param scoreLogFilePath
+   * @return プレイヤーログデータ
+   */
+  private static List<String> gameScoreLogData(Path gameScoreLogPath) throws IOException {
+    try (Stream<String> lines = Files.lines(gameScoreLogPath)) {
+      return lines.collect(Collectors.toList());
+    }
+  }
+
+  /**
+   * playerId毎にスコアを集計したプレイヤーログデータを取得
+   * 
+   * @param scoreLogFilePath
+   * @return プレイヤーログデータ
+   */
+  private static Map<String, Integer> getPlayLogData(List<String> scoreLogData) throws IOException {
+    return scoreLogData.stream().skip(1) // header
+        .map(line -> line.split(",")).collect(
+            groupingBy(values -> values[1], summingInt(values -> Integer.parseInt(values[2]))));
+  }
+
+  /**
+   * ログデータのソート
+   * 
+   * @param playerLogData
+   * @return スコアの降順でソートした結果
+   */
+  private static Map<String, Integer> sortPlayLogData(Map<String, Integer> playerLogData) {
+    return playerLogData.entrySet().stream()
+        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed()
+            .thenComparing(Map.Entry.<String, Integer>comparingByKey()))
+        .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (oldVal, newVal) -> oldVal,
+            LinkedHashMap::new));
+  }
+
+  /**
+   * ランキングデータの取得
+   * 
+   * @param playerLogData
+   * @return
+   */
+  private static List<String> getRankingData(Map<String, Integer> playerLogData,
+      Map<String, String> gameEntryLogData) {
+    int rank = 0;
+    int rankScore = 0;
+    List<String> rankingData = new ArrayList<>();
+    for (Map.Entry<String, Integer> playlog : playerLogData.entrySet()) {
+      if (rankScore == 0 || playlog.getValue() < rankScore) {
+        rank += 1;
+        rankScore = playlog.getValue();
+      }
+      if (rank > 10) {
+        break;
+      }
+      rankingData.add(rank + "," + playlog.getKey() + "," + gameEntryLogData.get(playlog.getKey())
+          + "," + playlog.getValue());
+    }
+    return rankingData;
+  }
+
+  private static void outputRankingData(List<String> rankingData) throws IOException {
+    System.out.println("rank,player_id,handle_name,score");
+    rankingData.forEach(line -> System.out.println(line));
+  }
+
+}
---
+++ src/test/ranking/v2/RankingTest.java
@@ -0,0 +1,43 @@
+package test.ranking.v2;
+
+import org.junit.jupiter.api.function.Executable;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.junit.jupiter.api.Test;
+
+import ranking.v2.Ranking;
+
+class RankingTest {
+
+  @Test
+  void mainTest_³íŒn() throws IOException {
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+
+    String[] args =
+        {"./src/test/ranking/v2/game_ently_log.csv", "./src/test/ranking/v2/game_score_log.csv"};
+    Ranking.main(args);
+
+    Path path = Path.of("./src/test/ranking/v2/expMainTest.csv");
+    String exp = Files.readString(path);
+    assertEquals(exp, out.toString());
+  }
+
+  @Test
+  void mainTest_ˆÙíŒn_ˆø”NULL() throws IOException {
+    NullPointerException e = assertThrows(NullPointerException.class, () -> {
+      Ranking.main(null);
+    });
+    assertEquals("Cannot load from object array because \"args\" is null", e.getMessage());
+  }
+
+}
