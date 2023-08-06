cfda1fd70d4eeecf907fb4faafeb65030fc356b2
tarosuke <felis.silvestris.catus.7777@gmail.com>
2023-08-06 16:32:00 +0900

    refactor

--- src/ranking/v1/Ranking.java
+++ src/ranking/v1/Ranking.java
@@ -1,4 +1,4 @@
-package ranking;
+package ranking.v1;
 import static java.util.stream.Collectors.groupingBy;
 import static java.util.stream.Collectors.summingInt;
 import static java.util.stream.Collectors.toMap;
@@ -15,9 +15,9 @@ public class Ranking {
 
 	public static void main(String[] args) throws IOException {
 		
-		Path scoreLogData = Paths.get(args[0]);
+		var playerLogFilePath = Paths.get(args[0]);
 				
-		Map<String, Integer> playerLogData = getPlayLogData(scoreLogData);
+		Map<String, Integer> playerLogData = getPlayLogData(playerLogFilePath);
 		
 		playerLogData = sortPlayLogData(playerLogData);
 		
@@ -33,7 +33,7 @@ public class Ranking {
 		System.out.println(sb.toString());
 	}
 
-	protected static String getRankingData(Map<String, Integer> sortedPlayerIdWithSumScore) {
+	private static String getRankingData(Map<String, Integer> sortedPlayerIdWithSumScore) {
 		int rank = 0;
 		int rankScore = 0;
 		StringBuilder sb = new StringBuilder();
@@ -51,21 +51,21 @@ public class Ranking {
 		return sb.toString();
 	}
 
-	protected static Map<String, Integer> sortPlayLogData(Map<String, Integer> playerIdWithSumScore) {
+	private static Map<String, Integer> sortPlayLogData(Map<String, Integer> playerIdWithSumScore) {
 		return playerIdWithSumScore.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
 				.collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
 	}
 
-	protected static Map<String, Integer> getPlayLogData(Path path) throws IOException {
-		try (Stream<String> stream = Files.lines(path)) {
-			return stream.skip(1) // header
+	private static Map<String, Integer> getPlayLogData(Path playerLogFilePath) throws IOException {
+		try (Stream<String> data = Files.lines(playerLogFilePath)) {
+			return data.skip(1) // header
 					.map(line -> line.split(","))
-					.collect(groupingBy(str -> str[1], summingInt(str -> Integer.parseInt(str[2]))));
+					.collect(groupingBy(values -> values[1], summingInt(values -> Integer.parseInt(values[2]))));
 
-		}
+		} 
 	}
 
-	protected static boolean isRankDown(int rankScore, Map.Entry<String, Integer> sorted) {
+	private static boolean isRankDown(int rankScore, Map.Entry<String, Integer> sorted) {
 		return rankScore == 0 || rankScore > sorted.getValue();
 	}
 
--- src/test/ranking/v1/RankingTest.java
+++ src/test/ranking/v1/RankingTest.java
@@ -1,4 +1,4 @@
-package test.ranking;
+package test.ranking.v1;
 import static org.junit.jupiter.api.Assertions.*;
 
 import java.io.ByteArrayOutputStream;
@@ -9,7 +9,7 @@ import java.nio.file.Path;
 
 import org.junit.jupiter.api.Test;
 
-import ranking.Ranking;
+import ranking.v1.Ranking;
 
 class RankingTest {
 
@@ -19,10 +19,10 @@ class RankingTest {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(out));
 		
-		String[] args = {"./src/test/ranking/score_log.csv"};
+		String[] args = {"./src/test/ranking/v1/score_log.csv"};
 		Ranking.main(args);
 		
-		Path path = Path.of("./src/test/ranking/expMainTest.csv");
+		Path path = Path.of("./src/test/ranking/v1/expMainTest.csv");
 		String exp = Files.readString(path);
 		assertEquals(exp, out.toString());
 	}
