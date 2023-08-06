bdcf6e251890110ac772013d07137d6f5a906e97
tarosuke <felis.silvestris.catus.7777@gmail.com>
2023-08-06 19:37:34 +0900

    format(GoogleStyle)

--- src/ranking/v1/Ranking.java
+++ src/ranking/v1/Ranking.java
@@ -17,71 +17,73 @@ import java.util.stream.Stream;
 
 public class Ranking {
 
-	public static void main(String[] args) throws IOException {
-		
-		var scoreLogFilePath = Paths.get(args[0]);
+  public static void main(String[] args) throws IOException {
 
-		Map<String, Integer> playerLogData = getPlayLogData(scoreLogFilePath);
+    var scoreLogFilePath = Paths.get(args[0]);
 
-		playerLogData = sortPlayLogData(playerLogData);
+    Map<String, Integer> playerLogData = getPlayLogData(scoreLogFilePath);
 
-		List<String> rankingData = getRankingData(playerLogData);
+    playerLogData = sortPlayLogData(playerLogData);
 
-		outputRankingData(rankingData);
-	}
+    List<String> rankingData = getRankingData(playerLogData);
 
-	/**
-	 * playerId毎にスコアを集計したプレイヤーログデータを取得
-	 * 
-	 * @param scoreLogFilePath
-	 * @return プレイヤーログデータ
-	 */
-	private static Map<String, Integer> getPlayLogData(Path scoreLogFilePath) throws IOException {
-		try (Stream<String> data = Files.lines(scoreLogFilePath)) {
-			return data.skip(1) // header
-					.map(line -> line.split(","))
-					.collect(groupingBy(values -> values[1], summingInt(values -> Integer.parseInt(values[2]))));
+    outputRankingData(rankingData);
+  }
 
-		}
-	}
+  /**
+   * playerId毎にスコアを集計したプレイヤーログデータを取得
+   * 
+   * @param scoreLogFilePath
+   * @return プレイヤーログデータ
+   */
+  private static Map<String, Integer> getPlayLogData(Path scoreLogFilePath) throws IOException {
+    try (Stream<String> data = Files.lines(scoreLogFilePath)) {
+      return data.skip(1) // header
+          .map(line -> line.split(",")).collect(
+              groupingBy(values -> values[1], summingInt(values -> Integer.parseInt(values[2]))));
 
-	/**
-	 * ログデータのソート
-	 * 
-	 * @param playerLogData
-	 * @return スコアの降順でソートした結果
-	 */
-	private static Map<String, Integer> sortPlayLogData(Map<String, Integer> playerLogData) {
-		return playerLogData.entrySet().stream().sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
-				.collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (oldVal, newVal) -> oldVal, LinkedHashMap::new));
-	}
+    }
+  }
 
-	/**
-	 * ランキングデータの取得
-	 * 
-	 * @param playerLogData
-	 * @return
-	 */
-	private static List<String> getRankingData(Map<String, Integer> playerLogData) {
-		int rank = 0;
-		int rankScore = 0;
-		List<String> rankingData = new ArrayList<>();
-		for (Map.Entry<String, Integer> sorted : playerLogData.entrySet()) {
-			if (rankScore == 0 || sorted.getValue() < rankScore) {
-				rank += 1;
-				rankScore = sorted.getValue();
-			}
-			if (rank > 10) {
-				break;
-			}
-			rankingData.add(rank + "," + sorted.getKey() + "," + sorted.getValue());
-		}
-		return rankingData;
-	}
+  /**
+   * ログデータのソート
+   * 
+   * @param playerLogData
+   * @return スコアの降順でソートした結果
+   */
+  private static Map<String, Integer> sortPlayLogData(Map<String, Integer> playerLogData) {
+    return playerLogData.entrySet().stream()
+        .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+        .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (oldVal, newVal) -> oldVal,
+            LinkedHashMap::new));
+  }
 
-	private static void outputRankingData(List<String> rankingData) throws IOException {	
-		System.out.println("rank,id,totalScore");
-		rankingData.forEach(line -> System.out.println(line));
-	}
+  /**
+   * ランキングデータの取得
+   * 
+   * @param playerLogData
+   * @return
+   */
+  private static List<String> getRankingData(Map<String, Integer> playerLogData) {
+    int rank = 0;
+    int rankScore = 0;
+    List<String> rankingData = new ArrayList<>();
+    for (Map.Entry<String, Integer> sorted : playerLogData.entrySet()) {
+      if (rankScore == 0 || sorted.getValue() < rankScore) {
+        rank += 1;
+        rankScore = sorted.getValue();
+      }
+      if (rank > 10) {
+        break;
+      }
+      rankingData.add(rank + "," + sorted.getKey() + "," + sorted.getValue());
+    }
+    return rankingData;
+  }
+
+  private static void outputRankingData(List<String> rankingData) throws IOException {
+    System.out.println("rank,id,totalScore");
+    rankingData.forEach(line -> System.out.println(line));
+  }
 
 }
--- src/test/ranking/v1/RankingTest.java
+++ src/test/ranking/v1/RankingTest.java
@@ -1,4 +1,5 @@
 package test.ranking.v1;
+
 import org.junit.jupiter.api.function.Executable;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -16,24 +17,26 @@ import ranking.v1.Ranking;
 
 class RankingTest {
 
-	@Test
-	void mainTest_³íŒn() throws IOException {
-		
-		ByteArrayOutputStream out = new ByteArrayOutputStream();
-		System.setOut(new PrintStream(out));
-		
-		String[] args = {"./src/test/ranking/v1/score_log.csv"};
-		Ranking.main(args);
-		
-		Path path = Path.of("./src/test/ranking/v1/expMainTest.csv");
-		String exp = Files.readString(path);
-		assertEquals(exp, out.toString());
-	}
-	
-	@Test
-	void mainTest_ˆÙíŒn_ˆø”NULL() throws IOException {
-		NullPointerException e = assertThrows(NullPointerException.class, () -> {Ranking.main(null);});
-		assertEquals("Cannot load from object array because \"args\" is null", e.getMessage());
-	}
-	
+  @Test
+  void mainTest_³íŒn() throws IOException {
+
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+    System.setOut(new PrintStream(out));
+
+    String[] args = {"./src/test/ranking/v1/score_log.csv"};
+    Ranking.main(args);
+
+    Path path = Path.of("./src/test/ranking/v1/expMainTest.csv");
+    String exp = Files.readString(path);
+    assertEquals(exp, out.toString());
+  }
+
+  @Test
+  void mainTest_ˆÙíŒn_ˆø”NULL() throws IOException {
+    NullPointerException e = assertThrows(NullPointerException.class, () -> {
+      Ranking.main(null);
+    });
+    assertEquals("Cannot load from object array because \"args\" is null", e.getMessage());
+  }
+
 }
