6c77449405a97defbb9d2dc4df276c88c865cc30
tarosuke <felis.silvestris.catus.7777@gmail.com>
2024-01-31 09:08:36 +0900

    record化の漏れを対応

--- src/ranking/v1_1/Ranking.java
+++ src/ranking/v1_1/Ranking.java
@@ -1,31 +1,7 @@
 package ranking.v1_1;
 
-public class Ranking {
-
-  private int rank;
-  private String playerId;
-  private int score;
-
-  public Ranking(int rank, String playerId, int score) {
-    this.rank = rank;
-    this.playerId = playerId;
-    this.score = score;
-  }
-
-  public int getRank() {
-    return this.rank;
-  }
-
-  public String getPlayerId() {
-    return this.playerId;
-  }
-
-  public int getScore() {
-    return this.score;
-  }
-
+public record Ranking(int rank, String playerId, int score) {
   public String toCsv() {
     return this.rank + "," + this.playerId + "," + this.score;
   }
-
 }
